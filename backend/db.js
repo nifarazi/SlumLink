b5788f5923d719b078d2e99604ff63246e8fe22a
ashfiatabassum <ashfiatabassum3897@gmail.com>
2026-01-27 01:34:23 +0600

    backend

--- backend/db.js
+++ backend/db.js
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "nanjiba@282002",
+  password: process.env.DB_PASSWORD || "mirpurdohs832",
   database: process.env.DB_NAME || "slumlink",
   waitForConnections: true,
   connectionLimit: 10,
