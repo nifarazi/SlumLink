2e9a8038a77f14791523eaa135b0cb87e9ab6df5
ZawadRafid <zawadurrafid7@gmail.com>
2026-02-25 20:49:35 +0600

    Fixed_DB

--- backend/db.js
+++ backend/db.js
@@ -9,11 +9,11 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, ".env") });
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "mirpurdohs832",
-  database: process.env.DB_NAME || "slumlink",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
